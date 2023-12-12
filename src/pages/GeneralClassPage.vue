1a83ebf8884dbde6670f11b99ab309397db408ae
Bestian Tang <bestian@gmail.com>
2023-12-12 13:18:23 +0800

    close #17

--- src/pages/GeneralClassPage.vue
+++ src/pages/GeneralClassPage.vue
@@ -1,43 +1,30 @@
 <template>
   <q-page class="flex flex-center" padding>
     <div class="text-h6 red">
-      {{
-        $t(purify(courseItems && courseItems[cid] && courseItems[cid].title))
-      }}
+      {{ $t(courseItems && courseItems[cid] && courseItems[cid].title) }}
     </div>
+    <p v-if="courseItems && courseItems[cid] && !courseItems[cid].useHTML">
+      {{ $t(courseItems && courseItems[cid] && courseItems[cid].description) }}
+    </p>
+    <p v-else v-html="$t(courseItems[cid].description)"></p>
   </q-page>
 </template>
 
 <script>
-import { watch } from "vue";
-import { defineComponent, ref } from "vue";
+import { defineComponent, computed } from "vue";
 import { useRoute } from "vue-router";
-
-const route = useRoute();
-console.log(route);
 
 export default defineComponent({
   name: "IndexPage",
   props: ["courseItems"],
   setup() {
-    watch(
-      () => route.params.cid,
-      async (newId) => {
-        console.log(newId);
-        cid.value = newId; // 這裡的 newId 是字串
-      }
-    );
+    const route = useRoute();
+    const cid = computed(() => route.params.cid);
     return {
-      cid: ref(""),
+      cid,
     };
   },
-  methods: {
-    // 如果輸入的是字串就傳回它本身，如果不是就傳回空字串
-    purify(str) {
-      console.log(str);
-      const ans = typeof str === "string" ? str : "";
-    },
-  },
+  methods: {},
 });
 </script>
 
