322f9338a517edbd28c6b507f1a4436cb094d366
frankguo0922 <b28228250@gmail.com>
2023-12-13 16:54:09 +0800

    建置表單的雛型

--- src/pages/GeneralClassPage.vue
+++ src/pages/GeneralClassPage.vue
@@ -4,7 +4,18 @@ q-page#c-page.flex.flex-start-center
     q-tabs.fluid.bg-secondary.text-white.shadow-2(v-model="tab", dense, align="justify", :breakpoint="0")
       q-tab(name="info", icon="info")
       q-tab(name="join", icon="shopping_cart")
-  .row(v-show="tab == 'join'")
+  .row.fluid(v-show="tab == 'join'")
+    .col-12
+      q-form.padding
+        q-input(v-model="name", :placeholder="$t('enter_your_name')")
+        q-input(v-model="phone", :placeholder="$t('enter_your_phone')")
+        q-input(v-model="email", :placeholder="$t('enter_your_email')")
+        q-input(type="number", v-model="price")
+      .row
+        .col-6
+          q-btn.fluid(size="lg" color="negative", icon="arrow_back_ios", @click="tab='info'")
+            | {{$t('goback')}} &nbsp;&nbsp;
+          .col-6
   .row(v-show="tab == 'info'")
     .col-12
       #header.text-h6 {{ $t(courseItems &amp;&amp; courseItems[cid] &amp;&amp; courseItems[cid].title) }}
@@ -13,6 +24,11 @@ q-page#c-page.flex.flex-start-center
         | $t(courseItems &amp;&amp; courseItems[cid] &amp;&amp; courseItems[cid].description)
         | }}
       p(v-else, v-html="$t(courseItems[cid].description)")
+      .row
+          .col-8
+          .col-4
+            q-btn.fluid(size="lg" color="positive", icon-right="send", @click="tab='join'")
+             | {{$t('join')}} &nbsp;&nbsp;
 </template>
 
 <script>
@@ -28,6 +44,10 @@ export default defineComponent({
     return {
       cid,
       tab: ref("info"),
+      name: ref(""),
+      phone: ref(""),
+      email: ref(""),
+      price: ref(1500),
     };
   },
   methods: {},
