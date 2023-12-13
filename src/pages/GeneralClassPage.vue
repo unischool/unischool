d0884be08a8437f944440c788b3fea88d1c65f51
frankguo0922 <b28228250@gmail.com>
2023-12-13 16:20:25 +0800

    change GeneralClassPage to pug

--- src/pages/GeneralClassPage.vue
+++ src/pages/GeneralClassPage.vue
@@ -1,39 +1,18 @@
-<template>
-  <q-page id="c-page" class="flex flex-start-center">
-    <div class="fluid row">
-      <q-tabs
-        v-model="tab"
-        dense
-        align="justify"
-        class="fluid bg-secondary text-white shadow-2"
-        :breakpoint="0"
-      >
-        <q-tab name="info" icon="info" />
-        <q-tab name="join" icon="shopping_cart" />
-      </q-tabs>
-    </div>
-    <div class="row" v-show="tab == 'join'">
-      <!--
-        https://quasar.dev/vue-components/form
-      -->
-    </div>
-    <div class="row" v-show="tab == 'info'">
-      <div class="col-12">
-        <div id="header" class="text-h6">
-          {{ $t(courseItems && courseItems[cid] && courseItems[cid].title) }}
-        </div>
-        <p
-          class="p-text"
-          v-if="courseItems && courseItems[cid] && !courseItems[cid].useHTML"
-        >
-          {{
-            $t(courseItems && courseItems[cid] && courseItems[cid].description)
-          }}
-        </p>
-        <p v-else v-html="$t(courseItems[cid].description)"></p>
-      </div>
-    </div>
-  </q-page>
+<template lang="pug">
+q-page#c-page.flex.flex-start-center
+  .fluid.row
+    q-tabs.fluid.bg-secondary.text-white.shadow-2(v-model="tab", dense, align="justify", :breakpoint="0")
+      q-tab(name="info", icon="info")
+      q-tab(name="join", icon="shopping_cart")
+  .row(v-show="tab == 'join'")
+  .row(v-show="tab == 'info'")
+    .col-12
+      #header.text-h6 {{ $t(courseItems &amp;&amp; courseItems[cid] &amp;&amp; courseItems[cid].title) }}
+      p.p-text(v-if="courseItems && courseItems[cid] && !courseItems[cid].useHTML")
+        | {{
+        | $t(courseItems &amp;&amp; courseItems[cid] &amp;&amp; courseItems[cid].description)
+        | }}
+      p(v-else, v-html="$t(courseItems[cid].description)")
 </template>
 
 <script>
