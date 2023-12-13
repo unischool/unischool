15ed01ed935080b57c6c69ef8ea1201f9593fc70
frankguo0922 <92869462+frankguo0922@users.noreply.github.com>
2023-12-13 13:04:55 +0800

    Update GeneralClassPage.vue

--- src/pages/GeneralClassPage.vue
+++ src/pages/GeneralClassPage.vue
@@ -1,9 +1,12 @@
 <template>
   <q-page class="flex flex-center" padding>
-    <div class="text-h6 red">
+    <div class="text-h6">
       {{ $t(courseItems && courseItems[cid] && courseItems[cid].title) }}
     </div>
-    <p v-if="courseItems && courseItems[cid] && !courseItems[cid].useHTML">
+    <p
+      class="p-text"
+      v-if="courseItems && courseItems[cid] && !courseItems[cid].useHTML"
+    >
       {{ $t(courseItems && courseItems[cid] && courseItems[cid].description) }}
     </p>
     <p v-else v-html="$t(courseItems[cid].description)"></p>
@@ -29,7 +32,21 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.red {
-  color: red;
+.text-h6 {
+  color: rgb(68, 68, 68);
+  font-weight: bold;
+  font-size: 35px;
+  margin: 20px;
+}
+.flex {
+  background: linear-gradient(to bottom, #fff2c0, #ffd9d9);
+}
+.p-text {
+  background-color: #fcffed;
+  border-radius: 10px;
+  padding: 10px;
+  padding-top: 0px;
+  line-height: 35px;
+  box-shadow: 5px 5px 5px rgb(187, 187, 187);
 }
 </style>
