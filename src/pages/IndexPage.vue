b0b809b87e7ce414c59d0c7fd7b88c7da91157b6
frankguo0922 <b28228250@gmail.com>
2023-12-14 11:52:47 +0800

    update pug

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,6 @@
-<template>
-  <q-page class="flex flex-center">
-    <q-img
-      alt="至青字宙学校"
-      src="~assets/logo.png"
-      style="max-width: 62vw"
-    ></q-img>
-  </q-page>
+<template lang="pug">
+q-page.flex.flex-center
+  q-img(alt="至青字宙学校", src="~assets/logo.png", style="max-width: 62vw")
 </template>
 
 <script>
