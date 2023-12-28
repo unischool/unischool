531563220a7902a833962451bd594738cb7f2bf2
leechiuhui <107933615+leechiuhui@users.noreply.github.com>
2023-12-28 12:41:45 +0800

    logo.png  style 82vmin 改為  62vh(第二次會議時老師有改，但發現不知為何origin沒改)

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
 q-page.flex.flex-center.index-wrap
 
 
-  q-img(alt="至青字宙学校", src="~assets/logo.png", style="max-width: 82vmin")
+  q-img(alt="至青字宙学校", src="~assets/logo.png", style="max-width: 62vh")
   q-img(alt="至青字宙学校 QR Code", src="~assets/sc3_qrcode_bit.ly_48sZXDm.png", style="max-width: 23vh")
 
 </template>
