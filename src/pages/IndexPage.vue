a6a5b18770c0c56514336ab130a63b924b16e7e1
leechiuhui <107933615+leechiuhui@users.noreply.github.com>
2023-12-28 11:09:09 +0800

    update qrcode
    from qrcode.png to sc3_qrcode_bit.ly_48sZXDm.png

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
 q-page.flex.flex-center.index-wrap
+<<<<<<< Updated upstream
   q-img(alt="至青字宙学校", src="~assets/logo.png", style="max-width: 62vh")
   q-img(alt="至青字宙学校 QR Code", src="~assets/qrcode.png", style="max-width: 23vh")
+=======
+  q-img(alt="至青字宙学校", src="~assets/logo.png", style="max-width: 82vmin")
+  q-img(alt="至青字宙学校 QR Code", src="~assets/sc3_qrcode_bit.ly_48sZXDm.png", style="max-width: 23vh")
+>>>>>>> Stashed changes
 </template>
 
 <script>
